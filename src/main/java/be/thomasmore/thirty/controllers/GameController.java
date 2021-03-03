28c94c774dee39efc7212360b108f50d8c5a4a2b
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-03-03 20:14:35 +0100

    Moved shipcreator to it's own file

--- src/main/java/be/thomasmore/thirty/controllers/GameController.java
+++ src/main/java/be/thomasmore/thirty/controllers/GameController.java
@@ -17,28 +17,9 @@ public class GameController {
     @Autowired
     private ShipRepository shipRepository;
     private Board board;
+    private ShipCreator shipCreator = new ShipCreator();
     private int imgSize = 20;
-    private boolean isFirstRun = true;
-
     private Point currentLocation = new Point(0, 0);
-
-    /*@GetMapping("game")
-    public String game(Model model) {
-        //onFirstRun();
-        //board = new Board();
-        model.addAttribute("started", false);
-        //model.addAttribute(board);
-        model.addAttribute("tileSize", imgSize);
-        return "game";
-    }*/
-
-    /*private void onFirstRun(){
-        if(isFirstRun){
-            //ImageWriter.writeImage(imgSize, imgSize);
-            board = new Board();
-            isFirstRun = false;
-        }
-    }*/
 
     @GetMapping(value="/tileClick")
     public void tileClick(@RequestParam("x") float rawX, @RequestParam("y") float rawY){
@@ -49,61 +30,21 @@ public class GameController {
     }
 
     @GetMapping("/game")
-    //@GetMapping(value="/start")
     public String start(@RequestParam(required = false) Integer width,
                         @RequestParam(required = false) Integer height,
                         Model model) {
         if(width == null || height == null){
-            //board = new Board();
             model.addAttribute("started", false);
-            //model.addAttribute(board);
-            //model.addAttribute("tileSize", imgSize);
             return "game";
         }
-    //public String start(Model model) {
-        //onFirstRun();
         System.out.print(width + height);
         board = new Board(width, height);
         Iterable<ShipClass> shipClasses = shipRepository.findAll();
-        ArrayList<Ship> ships = getShips(shipClasses);
+        ArrayList<Ship> ships = shipCreator.getShips(shipClasses, board);
         model.addAttribute(board);
         model.addAttribute("ships", ships);
         model.addAttribute("tileSize", imgSize);
         model.addAttribute("started", true);
         return "game";
-    }
-
-    private ArrayList<Ship> getShips(Iterable<ShipClass> shipClasses) {
-        ArrayList<Ship> ships = new ArrayList<>();
-        for(ShipClass shipClass : shipClasses){
-            for(int i = 0; i < shipClass.getAmount(); i++){
-                Ship ship = new Ship(shipClass, i);
-                ships.add(ship);
-                try {
-                    placeShip(ship);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        }
-        return ships;
-    }
-
-    private void placeShip(Ship ship) throws Exception {
-        Point[] segmentLocations = board.nextFreeShipSpace(ship.getShipSize());
-        board.place(ship, segmentLocations);
-    }
-
-    private void initSegments(Ship ship, Point[] points){
-        Segment[] segments = new Segment[points.length];
-        for(int i = 0; i < points.length; i++){
-            Point point = points[i];
-            try {
-                Tile tile = board.getTileAt(point);
-                ship.setSegmentTile(tile, i);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-        }
     }
 }
--- src/main/java/be/thomasmore/thirty/model/ShipCreator.java
+++ src/main/java/be/thomasmore/thirty/model/ShipCreator.java
@@ -1,7 +1,28 @@
 package be.thomasmore.thirty.model;
 
-public class ShipCreator {
-    public void putOnBoard(Board board, Ship ship){
+import java.awt.*;
+import java.util.ArrayList;
 
+public class ShipCreator {
+
+    public ArrayList<Ship> getShips(Iterable<ShipClass> shipClasses, Board board) {
+        ArrayList<Ship> ships = new ArrayList<>();
+        for(ShipClass shipClass : shipClasses){
+            for(int i = 0; i < shipClass.getAmount(); i++){
+                Ship ship = new Ship(shipClass, i);
+                ships.add(ship);
+                try {
+                    placeShip(ship, board);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ships;
+    }
+
+    private void placeShip(Ship ship, Board board) throws Exception {
+        Point[] segmentLocations = board.nextFreeShipSpace(ship.getShipSize());
+        board.place(ship, segmentLocations);
     }
 }
